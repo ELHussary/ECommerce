afd887a9f54e035eb03dfb239e7ac9e71835b729
ELHussary <malo.m25@yahoo.com>
2022-09-24 18:45:48 +0200

    Back to Windows

---
+++ api/database/migrations/2022_09_24_143000_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('country');
+            $table->string('full_name');
+            $table->integer('phone_number');
+            $table->string('street');
+            $table->string('governorate');
+            $table->string('city_area');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
